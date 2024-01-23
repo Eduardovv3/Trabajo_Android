3a3f73ffe47cfa3107590e1c17a0bb88a021f2e1
Eduardo Vivo <eduvivmed@alu.edu.gva.es>
2024-01-23 10:37:56 +0100

    itemproducto hecho

--- app/src/main/java/com/example/trabajo_android/MainActivity.kt
+++ app/src/main/java/com/example/trabajo_android/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.trabajo_android
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -13,7 +18,9 @@ import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -23,6 +30,7 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
@@ -44,8 +52,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.trabajo_android.ui.theme.Trabajo_AndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -108,6 +119,37 @@ fun MyTopAppBar() {
     )
 }
 
+@Composable
+fun Itemproducto(productos: Productos, onItemSelected: (Productos)-> Unit) {
+    Card(border = BorderStroke(2.dp, Color.Red),
+    modifier = Modifier.width(250.dp) .clickable { onItemSelected(productos)}){
+        Column(){
+            Image(
+                painter = painterResource(id = productos.Imagen),
+                contentDescription = "SuperHero Avatar",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = productos.Nombre,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+
+            )
+            Text(
+                text = productos.Ingredientes,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                fontSize = 12.sp
+            )
+            Text(
+                text = productos.Receta,
+                modifier = Modifier.align(Alignment.End).padding(6.dp),
+                fontSize = 10.sp
+            )
+
+        }
+
+    }
+}
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun MyContent(innerPadding: PaddingValues) {
